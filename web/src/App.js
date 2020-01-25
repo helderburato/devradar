ad9a40df64860020b3659d8f1fe60447b8e4209e
Helder Burato Berto <helder.burato@gmail.com>
2020-01-25 20:29:08 -0300

    feat(web/app): load DevForm, DevItem components

--- web/src/App.js
+++ web/src/App.js
@@ -1,36 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+
 import './global.css';
-import './main.css';
-import api from './services/api';
+import './app.css';
 import './sidebar.css';
+import './main.css';
+
+import api from './services/api';
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
   const [devs, setDevs] = useState([]);
-
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          const { latitude, longitude } = position.coords;
-
-          setLatitude(latitude);
-          setLongitude(longitude);
-        },
-        err => {
-          console.log(err);
-        },
-        {
-          timeout: 30000
-        }
-      );
-    }
-  }, []);
 
   useEffect(() => {
     async function loadDevs() {
@@ -42,18 +22,8 @@ function App() {
     loadDevs();
   }, []);
 
-  async function handleAddDev(event) {
-    event.preventDefault();
-
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGithubUsername('');
-    setTechs('');
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
 
     setDevs([...devs, response.data]);
   }
@@ -63,78 +33,13 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
 
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="username_github"
-              id="github_username"
-              value={github_username}
-              onChange={event => setGithubUsername(event.target.value)}
-              required
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              value={techs}
-              onChange={event => setTechs(event.target.value)}
-              required
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                value={latitude}
-                onChange={event => setLatitude(event.target.value)}
-                required
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                value={longitude}
-                onChange={event => setLongitude(event.target.value)}
-                required
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
           {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name} />
-
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-
-              <p>{dev.bio}</p>
-
-              <a href={`https://github.com/${dev.github_username}`}>
-                Acessar perfil no Github
-              </a>
-            </li>
+            <DevItem key={dev._id} dev={dev} />
           ))}
         </ul>
       </main>

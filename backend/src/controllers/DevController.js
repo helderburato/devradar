4bdc6882c9f154564a17bcc4663f1c0d8f28c10b
Helder Burato Berto <helder.burato@gmail.com>
2020-02-01 17:49:20 -0300

    feat(backend/send message): socket io send message to frontend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
-const { findConnections } = require('../websocket');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(req, res) {
@@ -46,7 +46,7 @@ module.exports = {
         techsArray
       );
 
-      console.log(sendSocketMessageTo);
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
     return res.json(dev);
--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -2,10 +2,11 @@ const socketio = require('socket.io');
 const parseStringAsArray = require('./utils/parseStringAsArray');
 const calculateDistance = require('./utils/calculateDistance');
 
+let io;
 const connections = [];
 
 exports.setupWebsocket = server => {
-  const io = socketio(server);
+  io = socketio(server);
 
   io.on('connection', socket => {
     const { latitude, longitude, techs } = socket.handshake.query;
@@ -27,5 +28,11 @@ exports.findConnections = (coordinates, techs) => {
       calculateDistance(coordinates, connection.coordinates) < 10 &&
       connection.techs.some(item => techs.includes(item))
     );
+  });
+};
+
+exports.sendMessage = (to, message, data) => {
+  to.forEach(connection => {
+    io.to(connection.id).emit(message, data);
   });
 };

b44cbc89bf6a687e261f2e394a2d95d1d54d6eec
Helder Burato Berto <helder.burato@gmail.com>
2020-02-01 16:33:17 -0300

    feat(search form): added to Main page the search form

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
 } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -36,31 +44,49 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -27.6027754, longitude: -48.5288091 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: 'https://avatars3.githubusercontent.com/u/862575?s=460&v=4'
-          }}
+    <>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
 
-        <Callout
-          onPress={() => {
-            navigation.navigate('Profile', { github_username: 'helderburato' });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Helder Burato Berto</Text>
-            <Text style={styles.devBio}>
-              Front-end Developer • Learning, practicing and creating this is my
-              motto 💥
-            </Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -27.6027754, longitude: -48.5288091 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://avatars3.githubusercontent.com/u/862575?s=460&v=4'
+            }}
+          />
+
+          <Callout
+            onPress={() => {
+              navigation.navigate('Profile', {
+                github_username: 'helderburato'
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Helder Burato Berto</Text>
+              <Text style={styles.devBio}>
+                Front-end Developer • Learning, practicing and creating this is
+                my motto 💥
+              </Text>
+              <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+    </>
   );
 }
 
@@ -93,6 +119,42 @@ const styles = StyleSheet.create({
 
   devTechs: {
     marginTop: 5
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row'
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#fff',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8e4dff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15
   }
 });
 
